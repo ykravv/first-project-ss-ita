7266fd95a0db94c978fd9e5823a5d66ffa9234bc
Mikel <mihail.ryabokon@gmail.com>
2013-02-19 19:25:50 +0200

    some changes

--- js/search_page/SearchController.js
+++ js/search_page/SearchController.js
@@ -1,11 +1,20 @@
 function SearchController() {
 	var search = new SearchResult();
 	
-	search.setAllCards(/*cards array*/);
-	search.getAllCards ();
+	self_controller = this;
+
+	this.startEasySearch = function () {
+		var fasade_obj = new Facade(),
+			fullname = search_fullname.value;
+		
+		//@param {array}
+		//@param {method of main_controller}
+		fasade_obj.sendSearchRequest(fullname, self_controller.callback);
 	
-  // This is a test comment!!!
-
-
-// :)
+	this.startExtendSearch = function (/*params*/) {
+	//params :
+	//select_age, age, sex, army, educationLevel, select_education	
+		var fasade_obj = new Facade(),
+	fasade_obj.sendSearchRequest(/*params*/, self_controller.callback);
+	}
 }
