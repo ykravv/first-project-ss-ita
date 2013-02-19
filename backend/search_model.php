d29fa99dee3309b74d3960b48c0ba31e042a97a1
Evgeny Kravchenko <evgeniy.kravtschenko@gmail.com>
2013-02-19 21:33:17 +0200

    Added SearchController for backend with method SearchCards()

--- backend/search_model.php
+++ backend/search_model.php
@@ -12,6 +12,9 @@ class SearchModel
     $facade = new Facade();  
     $result_search = $facade->SearchCards($search_string);
     foreach ($result_search as $one) {
+      
+
+
       // $education = $facade->getModel($one["id"], "education"); // id, table ??
       // $post_education = $facade->getModel($one["id"], "post_education"); 
       // $family = $facade->getModel($one["id"], "family"); 
