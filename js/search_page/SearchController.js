4f2f8bdddd291c221b98604843281089e573a56a
SinichkinaK <sinichkinakate@gmail.com>
2013-02-20 21:40:11 +0200

    html+button filter and view JS

--- js/search_page/SearchController.js
+++ js/search_page/SearchController.js
@@ -1,16 +1,13 @@
 autoload("js/search_page/searchResult.js");
 //TEST
 /*var obj = new SearchController();
-var array = new Array();//need array
+var array = new Array(["IIIIII","iiiiiiii","1111111111111"],["EEEEEEEEE","eeeeeeeeee","22222222222"],["AAAAAAA","aaaaaaaa","333333333"]);//need array
 obj.viewListCards(array);*/
 //--endTEST
 
 function SearchController() {
-	var search = new SearchResult();
-	self_controller = this;
-
+ 
   var search = new SearchResult();
-  var cards_array;
   self_controller = this;
 
   this.startEasySearch = function () {
@@ -26,7 +23,7 @@ function SearchController() {
   this.callback = function(data){
     console.log(data);
   }
-  //
+  
   this.startExtendSearch = function (select_age, age, sex, army, educationLevel, select_education) {
     var param_arr = [select_age, age, sex, army, educationLevel, select_education];
     search.setAllCards(param_arr);
@@ -35,39 +32,30 @@ function SearchController() {
   
   //view list of cards
   this.viewListCards = function(cards_array)  {
-    var i;        
+    var i, card_string;   
+    var users = document.getElementById('users');
     
     i=0;
-    for(cards_array in object_card)
+    for(object_card in cards_array)
     {   
-      for(object_card in key)
+      card_string=""; 
+      var li = document.createElement("li");
+      li.setAttribute ("id","card_"+i);
+      for(key in cards_array[object_card])
       { 
-        var li = document.createElement("li");
-        li.setAttribute ("id","card_"+i);
-        //text of one card
-        li.innerHTML = object_card[key];          
-        users.appendChild(li);
+        if(key<3)
+        {
+          //text of one card
+          card_string += cards_array[object_card][key]+" ";          
+        }
+        else
+          break;
       }
+      li.innerHTML = card_string+"<br />";
+      users.appendChild(li);
       i++;
     }
-      /*
-    var i,j;
-    i=0;
-    for(cards_array in object_card)
-    {          
-      j=0;
-      
-      foreach(object_card in card)
-      { 
-        foreach(card in key[value] )
-        { 
-                       
-        }  
-        j++;
-      }  
-      i++;
-    }*/
-  }
+   }
   //list cards from model
   this.listCards = function()  {
     var cards_array = search.getAllCards;
